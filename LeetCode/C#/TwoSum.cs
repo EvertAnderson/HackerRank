9d48db77527cb10a53bf3b2e7a1891d589359069
EVERT-ANDERSON\Evert <evert.anderson.hl@gmail.com>
2024-02-24 22:05:33 -0500

    Update Two Sum

--- LeetCode/C#/TwoSum.cs
+++ LeetCode/C#/TwoSum.cs
@@ -1,26 +1,19 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for(int i=0; i<nums.Length; i++)
+        for (int i = 0; i < nums.Length; i++)
         {
             int tempTarget = target;
-            if(nums[i] < tempTarget)
+            tempTarget -= nums[i];
+            for (int j = i + 1; j < nums.Length; j++)
             {
-                tempTarget -= nums[i];
-                for(int j=i+1; j<nums.Length; j++)
+                if (nums[j] == tempTarget)
                 {
-                    if(nums[j] == tempTarget)
-                    {
-                        result[0] = i;
-                        result[1] = j;
-                        tempTarget = 0;
-                        break;
-                    }
+                    return [i,j];
                 }
             }
-            if(tempTarget == 0) break;
+            if (tempTarget == 0) break;
         }
 
-        return result;
+        return [0,0];
     }
 }
